e9b0425ebea72580fda6ea05a4567098f0886fd0
Roger Franklin <rogerfranklin@outlook.com>
2018-10-19 10:04:34 +0100

    Add knobs to storybook

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,20 +2,25 @@ import React from 'react';
 import { ThemeProvider } from 'emotion-theming';
 import { storiesOf } from '@storybook/react';
 import { Button, Link, theme } from '../src';
+import { text } from '@storybook/addon-knobs';
 
-storiesOf('Link', module).add('with text', () => (
+const linkStories = storiesOf('Link', module);
+const buttonStories = storiesOf('Button', module);
+
+linkStories.add('with text', () => (
   <ThemeProvider theme={theme}>
-    <Link href="goo.gl">Hello Link</Link>
+    <Link href={text('href', 'goo.gl')}>{text('Label', 'Hello Link')}</Link>
   </ThemeProvider>
 ));
-storiesOf('Button', module)
-  .add('Primary', () => (
-    <ThemeProvider theme={theme}>
-      <Button type="primary">Primary</Button>
-    </ThemeProvider>
-  ))
-  .add('Secondary', () => (
-    <ThemeProvider theme={theme}>
-      <Button type="secondary">Secondary</Button>
-    </ThemeProvider>
-  ));
+
+buttonStories.add('Primary', () => (
+  <ThemeProvider theme={theme}>
+    <Button type="primary">{text('Children', 'Primary')}</Button>
+  </ThemeProvider>
+));
+
+buttonStories.add('Secondary', () => (
+  <ThemeProvider theme={theme}>
+    <Button type="secondary">{text('Children', 'Secondary')}</Button>
+  </ThemeProvider>
+));
