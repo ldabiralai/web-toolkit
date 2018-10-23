055fc9a139ecb6a5e305e893bb76bd112cd3f056
Lawrence Dabir-Alai <lawrence.dabir@gmail.com>
2018-10-23 10:11:33 +0100

    Add tests for link component and refactor enzyme configuration

--- src/atoms/Button/index.test.js
+++ src/atoms/Button/index.test.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
+import { shallow } from 'enzyme';
 import Button from '.';
 import { theme } from '../..';
-
-configure({ adapter: new Adapter() });
 
 it('renders a Button', () => {
   expect(shallow(<Button theme={theme}>Click me !</Button>)).toMatchSnapshot();
