dddf78e97ea75c731fadb2cb6fe661a195a5fa52
Lawrence Dabir-Alai <lawrence.dabir@gmail.com>
2018-11-07 16:56:59 +0000

    Add test to ensure that linkProps are added to each link correctly

--- src/modules/Footer/index.test.js
+++ src/modules/Footer/index.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import Footer from '.';
 
 const faq = {
-  label: 'Help & Faq',
+  label: 'Help & FAQs',
   linkProps: {
     href: 'https://google.fr',
   },
@@ -43,5 +43,38 @@ describe('Footer', () => {
 
   it('renders with no items', () => {
     expect(shallow(<Footer faq={faq} copyright="copyright" />)).toMatchSnapshot();
+  });
+
+  it('should allow extra attibutes to be passed to the faq link (eg for analytics to hook into)', () => {
+    const faqWithExtraAttibute = {
+      label: 'Help & FAQs',
+      linkProps: {
+        href: 'https://google.fr',
+        'data-adobe-analytics': 'an-adobe-analytics-value',
+      },
+    };
+
+    const component = shallow(<Footer faq={faqWithExtraAttibute} />);
+    const helpButton = component.find({ children: 'Help & FAQs' });
+
+    expect(helpButton.props()).toHaveProperty('data-adobe-analytics', 'an-adobe-analytics-value');
+  });
+
+  it('should allow extra attibutes to be passed to an item link (eg for analytics to hook into)', () => {
+    const itemWithExtraAttibute = [
+      {
+        id: 1,
+        label: 'Terms & Conditions',
+        linkProps: {
+          href: 'https://google.fr',
+          'data-adobe-analytics': 'an-adobe-analytics-value',
+        },
+      },
+    ];
+
+    const component = shallow(<Footer faq={faq} items={itemWithExtraAttibute} />);
+    const itemLink = component.find({ children: 'Terms & Conditions' });
+
+    expect(itemLink.props()).toHaveProperty('data-adobe-analytics', 'an-adobe-analytics-value');
   });
 });
