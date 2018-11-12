d926f759c43c21989b3d2791c01c99b761fd8a9f
igilfill <iain_gilfillan@discovery.com>
2018-11-12 10:43:52 +0000

    Updates tests

--- src/elements/Icon/index.js
+++ src/elements/Icon/index.js
@@ -65,7 +65,7 @@ const Icon = ({ type, alt, height, className}) =>
 
 Icon.defaultProps = {
   alt: '',
-  className: ''
+  className: '',
 };
 
 Icon.propTypes = {
--- src/elements/Icon/index.test.js
+++ src/elements/Icon/index.test.js
@@ -4,8 +4,8 @@ import Icon, { iconMap } from '.';
 
 describe('Icon', () => {
   it('should render source for a valid icon type', () => {
-    const wrapper = shallow(<Icon className="test" type="e1" height={50} />);
-    expect(wrapper.find('img').prop('src')).toEqual(iconMap.e1.src);
+    const wrapper = shallow(<Icon className="test" type="E1" height={50} />);
+    expect(wrapper.prop('src')).toEqual(iconMap.E1.src);
   });
 
   it('should not render with an invalid icon type', () => {
@@ -14,13 +14,13 @@ describe('Icon', () => {
   });
 
   it('should display alt text passed in from props', () => {
-    const wrapper = shallow(<Icon className="test" type="e1" alt="test-alt" height={50} />);
-    expect(wrapper.find('img').prop('alt')).toEqual('test-alt');
+    const wrapper = shallow(<Icon className="test" type="E1" alt="test-alt" height={50} />);
+    expect(wrapper.prop('alt')).toEqual('test-alt');
   });
 
   it('should display default alt text when not passed in props', () => {
-    const wrapper = shallow(<Icon className="test" type="e2" height={50} />);
-    expect(wrapper.find('img').prop('alt')).toEqual('Eurosport 2');
+    const wrapper = shallow(<Icon className="test" type="E2" height={50} />);
+    expect(wrapper.prop('alt')).toEqual('Eurosport 2');
   });
 
   it('should default width of icon to be the same as height when ratio is not configured for the icon type', () => {
@@ -29,12 +29,12 @@ describe('Icon', () => {
   });
 
   it('should apply correct width of icon when ratio is configured for the icon type', () => {
-    const wrapper = shallow(<Icon className="test" type="e2" height={50} />);
+    const wrapper = shallow(<Icon className="test" type="E2" height={50} />);
     expect(wrapper).toHaveStyleRule('width', '83px');
   });
 
   it('should apply icon height passed in through props', () => {
-    const wrapper = shallow(<Icon className="test" type="e2" height={50} />);
+    const wrapper = shallow(<Icon className="test" type="E2" height={50} />);
     expect(wrapper).toHaveStyleRule('height', '50px');
   });
 });
