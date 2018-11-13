bbd3bd4dd60146e877a9d565f64d312fd17f7326
Léopold Serdet <leopold.serdet@gmail.com>
2018-11-13 11:30:18 +0100

    Fix import to resolve storybook build issue

--- src/elements/Logo/index.stories.js
+++ src/elements/Logo/index.stories.js
@@ -1,6 +1,6 @@
 import { withInfo } from '@storybook/addon-info';
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import Logo from '.';
+import { Logo } from '../..';
 
 storiesOf('Logo', module).add('default', withInfo()(() => <Logo />));
