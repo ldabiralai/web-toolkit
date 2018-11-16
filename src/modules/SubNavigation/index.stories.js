0ec65278940fe41d1338e4adf4d33890bb422786
xavierboreux <xavier_boreux.external@discovery.com>
2018-11-16 11:20:52 +0100

    fix subnavigation import

--- src/modules/SubNavigation/index.stories.js
+++ src/modules/SubNavigation/index.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { object } from '@storybook/addon-knobs';
 import { withInfo } from '@storybook/addon-info';
-import SubNavigation from './index';
+import { SubNavigation } from '../..';
 
 const subNavigationStories = storiesOf('SubNavigation', module);
 const items = [
