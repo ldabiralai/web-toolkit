12d79042f483198f70ac5de36b1ebc0fbc886c85
rormrod <roland_ormrod@discovery.com>
2019-01-17 13:41:52 +0000

    Fixing failing tests for grid

--- src/elements/Grid/index.test.js
+++ src/elements/Grid/index.test.js
@@ -23,42 +23,44 @@ describe('renders the good styles for container', () => {
 describe('renders the good styles for column', () => {
   it('tiny breakpoint', () => {
     expect(getColumnStyles(1, 1, 'tiny')).toEqual(
-      'width: 11.324133572105762%;margin-left: 16.664910987043086%;padding: 0 2.6703887074686614%;'
+      'width: 16.664910987043086%;margin-left: 16.664910987043086%;padding: 0 2.6703887074686614%;'
     );
     expect(getColumnStyles(6, 0, 'tiny')).toEqual(
-      'width: 94.64868850732117%;margin-left: 0%;padding: 0 2.6703887074686614%;'
+      'width: 99.98946592225849%;margin-left: 0%;padding: 0 2.6703887074686614%;'
     );
     expect(getColumnStyles('full', 0, 'tiny')).toEqual(
-      'width: 94.64868850732117%;margin-left: 0%;padding: 0 2.6703887074686614%;'
+      'width: 99.98946592225849%;margin-left: 0%;padding: 0 2.6703887074686614%;'
     );
   });
   it('small breakpoint', () => {
     expect(getColumnStyles(1, 1, 'small')).toEqual(
-      'width: 12.013394725826707%;margin-left: 16.65969024696526%;padding: 0 2.323147760569276%;'
+      'width: 16.65969024696526%;margin-left: 16.65969024696526%;padding: 0 2.323147760569276%;'
     );
     expect(getColumnStyles(6, 0, 'small')).toEqual(
-      'width: 95.311845960653%;margin-left: 0%;padding: 0 2.323147760569276%;'
+      'width: 99.95814148179156%;margin-left: 0%;padding: 0 2.323147760569276%;'
     );
     expect(getColumnStyles('full', 0, 'small')).toEqual(
-      'width: 95.311845960653%;margin-left: 0%;padding: 0 2.323147760569276%;'
+      'width: 99.95814148179156%;margin-left: 0%;padding: 0 2.323147760569276%;'
     );
   });
   it('medium breakpoint', () => {
     expect(getColumnStyles(1, 1, 'medium')).toEqual(
-      'width: 5.834666666666666%;margin-left: 8.330666666666666%;padding: 0 1.248%;'
+      'width: 8.330666666666666%;margin-left: 8.330666666666666%;padding: 0 1.248%;'
     );
-    expect(getColumnStyles(12, 0, 'medium')).toEqual('width: 97.472%;margin-left: 0%;padding: 0 1.248%;');
-    expect(getColumnStyles('full', 0, 'medium')).toEqual('width: 97.472%;margin-left: 0%;padding: 0 1.248%;');
+    expect(getColumnStyles(12, 0, 'medium')).toEqual('width: 99.96799999999999%;margin-left: 0%;padding: 0 1.248%;');
+    expect(getColumnStyles('full', 0, 'medium')).toEqual(
+      'width: 99.96799999999999%;margin-left: 0%;padding: 0 1.248%;'
+    );
   });
   it('large breakpoint', () => {
-    expect(getColumnStyles(1, 1, 'large')).toEqual('width: 62px;margin-left: 82px;padding: 0 10px;');
-    expect(getColumnStyles(12, 0, 'large')).toEqual('width: 964px;margin-left: 0px;padding: 0 10px;');
-    expect(getColumnStyles('full', 0, 'large')).toEqual('width: 964px;margin-left: 0px;padding: 0 10px;');
+    expect(getColumnStyles(1, 1, 'large')).toEqual('width: 82px;margin-left: 82px;padding: 0 10px;');
+    expect(getColumnStyles(12, 0, 'large')).toEqual('width: 984px;margin-left: 0px;padding: 0 10px;');
+    expect(getColumnStyles('full', 0, 'large')).toEqual('width: 984px;margin-left: 0px;padding: 0 10px;');
   });
   it('wide breakpoint', () => {
-    expect(getColumnStyles(1, 1, 'wide')).toEqual('width: 90px;margin-left: 109px;padding: 0 9.5px;');
-    expect(getColumnStyles(12, 0, 'wide')).toEqual('width: 1289px;margin-left: 0px;padding: 0 9.5px;');
-    expect(getColumnStyles('full', 0, 'wide')).toEqual('width: 1289px;margin-left: 0px;padding: 0 9.5px;');
+    expect(getColumnStyles(1, 1, 'wide')).toEqual('width: 109px;margin-left: 109px;padding: 0 9.5px;');
+    expect(getColumnStyles(12, 0, 'wide')).toEqual('width: 1308px;margin-left: 0px;padding: 0 9.5px;');
+    expect(getColumnStyles('full', 0, 'wide')).toEqual('width: 1308px;margin-left: 0px;padding: 0 9.5px;');
   });
 });
 
