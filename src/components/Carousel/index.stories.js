a146fdb24e7c5b7b4159e8f8b12388df981cf2f9
Xavier Boreux <xavier_boreux.external@discovery.com>
2019-01-21 17:31:56 +0100

    Create carousel system

---
+++ src/components/Carousel/index.stories.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import styled from 'react-emotion';
+import { object } from '@storybook/addon-knobs';
+import Carousel from '.';
+import WatchbarCard from '../Card/WatchbarCard';
+
+const indexStories = storiesOf('Carousel', module);
+
+const Item = styled.div`
+  background: darkorange;
+  text-align: center;
+  padding: 50px;
+  color: white;
+  border: 1px solid grey;
+`;
+
+const base = {
+  img: 'https://i.eurosport.com/taiga/MagicBox/Crop/16_9/0_20180710-125830.jpeg?w=640',
+  url: '/article/id',
+  category: 'Tennis',
+  title: 'Klopp happy with Chelsea draw after good performance',
+  description: 'Day 2',
+  timestamp: '09:00 - 10:30',
+};
+
+const watchbar = {
+  ...base,
+  title: 'Darts: BDO World Championship',
+  channel: 'E1FR',
+  liveLabel: 'live',
+  isLive: true,
+};
+
+indexStories.add(
+  'Carousel',
+  withInfo()(() => (
+    <Carousel>
+      <WatchbarCard card={object('card', watchbar)} />
+      <WatchbarCard card={object('card', watchbar)} />
+      <WatchbarCard card={object('card', watchbar)} />
+      <WatchbarCard card={object('card', watchbar)} />
+      <WatchbarCard card={object('card', watchbar)} />
+      <WatchbarCard card={object('card', watchbar)} />
+      <WatchbarCard card={object('card', watchbar)} />
+      <WatchbarCard card={object('card', watchbar)} />
+      <WatchbarCard card={object('card', watchbar)} />
+    </Carousel>
+  ))
+);
