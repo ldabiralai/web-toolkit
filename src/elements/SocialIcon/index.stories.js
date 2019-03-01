ffedcd6f2276d2e502a36629d0085ace9cac976a
vborisenkov <vborisenkov@lohika.com>
2019-03-01 10:50:10 +0200

    add info decorator to the socialIcon element

--- src/elements/SocialIcon/index.stories.js
+++ src/elements/SocialIcon/index.stories.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { text, radios } from '@storybook/addon-knobs';
+import { withInfo } from '@storybook/addon-info';
 
 import SocialIcon from './index';
 
-const indexStories = storiesOf('SocialIcon', module);
+const indexStories = storiesOf('SocialIcon', module).addDecorator(withInfo);
 
 indexStories.add('configurable', () => (
   <SocialIcon
