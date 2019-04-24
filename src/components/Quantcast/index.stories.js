d0a0d741e2d2cdc03380ef4a64dfd21957427f31
Raphael <raroullet@gmail.com>
2019-04-24 15:01:06 +0200

    EWL-476-create-Quantcast-component (#110)
    * Update snapshot
    
    * Update snapshot
    
    * test CircleCI
    
    * Add object knob for translation
    
    * Update carousel snapshot
    
    * change knob name to config

--- src/components/Quantcast/index.stories.js
+++ src/components/Quantcast/index.stories.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import { text } from '@storybook/addon-knobs';
+import { text, object } from '@storybook/addon-knobs';
 import { Quantcast } from '../..';
-import translation from './mockData/quantcast-translation.json';
+import config from './mockData/quantcast-translation.json';
 
 const quantcastStories = storiesOf('Quantcast', module).addDecorator(withInfo);
 
 quantcastStories.add('Quantcast', () => (
-  <Quantcast scriptUrl={text('scriptUrl', 'https://quantcast.mgr.consensu.org/v14/cmp.js')} cmpConf={translation} />
+  <Quantcast
+    scriptUrl={text('scriptUrl', 'https://quantcast.mgr.consensu.org/v14/cmp.js')}
+    cmpConf={object('config', config)}
+  />
 ));
